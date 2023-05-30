63ae55f5e458aedcfc87b0da22f84748e7973dc2
omer faruk Ozer <ozeromerfaruk@gmail.com>
2023-05-30 19:38:42 +0300

    dice

---
+++ Assets/Scripts/Dice.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Dice : MonoBehaviour
+{
+    private void Update()
+    {
+        GetRandomNumber();
+    }
+
+    private static int GetRandomNumber()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            int randomNumber = Random.Range(1, 7);
+            print(randomNumber);
+            return randomNumber;
+        }
+
+        return 0;
+    }
+}
