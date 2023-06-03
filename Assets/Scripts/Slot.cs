f4eacb536c09467311be58678e19941d15d43057
omer faruk Ozer <104875927+omerozerf@users.noreply.github.com>
2023-06-03 23:32:56 +0300

    Update Slot.cs

--- Assets/Scripts/Slot.cs
+++ Assets/Scripts/Slot.cs
@@ -6,7 +6,7 @@ public class Slot : MonoBehaviour
 {
     private List<Stone> stoneList = new List<Stone>();
  
-
+ 
     public void AddStoneToList(Stone stone)
     {
         stoneList.Add(stone); 
